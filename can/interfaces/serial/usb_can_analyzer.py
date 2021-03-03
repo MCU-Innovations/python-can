88a3e8457915907cac46b34b28e716619dfd38cf
Ryan Hope <ryan.hope@mcuinnovations.com>
2021-03-03 15:21:38 -0500

    copy some setup from seeedstudiobus

--- can/interfaces/serial/usb_can_analyzer.py
+++ can/interfaces/serial/usb_can_analyzer.py
@@ -11,12 +11,73 @@ logger = logging.getLogger(__name__)
 
 
 class UsbCanAnalyzer(BusABC):
-    def __init__(self, channel, *args, **kwargs):
+
+    BITRATE = {
+        1000000: 0x01,
+        800000: 0x02,
+        500000: 0x03,
+        400000: 0x04,
+        250000: 0x05,
+        200000: 0x06,
+        125000: 0x07,
+        100000: 0x08,
+        50000: 0x09,
+        20000: 0x0A,
+        10000: 0x0B,
+        5000: 0x0C,
+    }
+
+    FRAMETYPE = {"STD": 0x01, "EXT": 0x02}
+
+    OPERATIONMODE = {
+        "normal": 0x00,
+        "loopback": 0x01,
+        "silent": 0x02,
+        "loopback_and_silent": 0x03,
+    }
+
+    def __init__(
+        self,
+        channel,
+        baudrate=2000000,
+        timeout=0.1,
+        frame_type="STD",
+        operation_mode="normal",
+        bitrate=500000,
+        *args,
+        **kwargs
+    ):
 
         self.channel_info = "USB-CAN Analyzer: " + channel
-        self.ser = serial.serial_for_url(channel, baudrate=2000000, bytesize=8, parity="N", stopbits=1, timeout=0.05)
+        self.bit_rate = bitrate
+        self.frame_type = frame_type
+        self.op_mode = operation_mode
+        self.filter_id = bytearray([0x00, 0x00, 0x00, 0x00])
+        self.mask_id = bytearray([0x00, 0x00, 0x00, 0x00])
+        self.ser = serial.serial_for_url(
+            channel, baudrate=baudrate, bytesize=8, parity="N", stopbits=1, timeout=timeout, rtscts=False
+        )
 
         super().__init__(channel=channel, *args, **kwargs)
+        self.init()
+
+    def init(self):
+        byte_msg = bytearray()
+        byte_msg.append(0xAA)  # Frame Start Byte 1
+        byte_msg.append(0x55)  # Frame Start Byte 2
+        byte_msg.append(0x12)  # Initialization Message ID
+        byte_msg.append(UsbCanAnalyzer.BITRATE[self.bit_rate])  # CAN Baud Rate
+        byte_msg.append(UsbCanAnalyzer.FRAMETYPE[self.frame_type])
+        byte_msg.extend(self.filter_id)
+        byte_msg.extend(self.mask_id)
+        byte_msg.append(UsbCanAnalyzer.OPERATIONMODE[self.op_mode])
+        byte_msg.append(0x01)  # Follows 'Send once' in windows app.
+        byte_msg.extend([0x00] * 4)  # Manual bitrate config, details unknown.
+        crc = sum(byte_msg[2:]) & 0xFF
+        byte_msg.append(crc)
+
+        self.ser.write(byte_msg)
+        self.ser.flush()
 
     def shutdown(self):
         self.ser.close()
