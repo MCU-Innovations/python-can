fa6953ef157f84da4bb12e24e2f4b9d9cc7bbd8c
Ryan Hope <ryan.hope@mcuinnovations.com>
2021-03-08 11:18:54 -0500

    more cleanups

--- can/interfaces/serial/usb_can_analyzer.py
+++ can/interfaces/serial/usb_can_analyzer.py
@@ -100,48 +100,48 @@ class UsbCanAnalyzer(BusABC):
         self.ser.reset_output_buffer()
 
     def send(self, msg, timeout=None):
-        byte_msg = bytearray()
-        byte_msg.append(0xAA)
-        byte_msg.append(0xC0 | (msg.is_extended_id << 5) | (msg.is_remote_frame << 4) | msg.dlc)
-        byte_msg += struct.pack("<I" if msg.is_extended_id else "<H", msg.arbitration_id)
-        byte_msg += msg.data
-        byte_msg.append(0x55)
-        self.ser.write(byte_msg)
-        self.ser.flush()
-        wait(0.0001)
+        try:
+            byte_msg = bytearray()
+            byte_msg.append(0xAA)
+            byte_msg.append(0xC0 | (msg.is_extended_id << 5) | (msg.is_remote_frame << 4) | msg.dlc)
+            byte_msg += struct.pack("<I" if msg.is_extended_id else "<H", msg.arbitration_id)
+            byte_msg += msg.data
+            byte_msg.append(0x55)
+            self.ser.write(byte_msg)
+            self.ser.flush()
+            wait(0.0002)
+        except serial.SerialException:
+            pass
 
     def _recv_internal(self, timeout):
         try:
             rx_byte = self.ser.read()
+            if rx_byte and ord(rx_byte) == 0xAA:
+                tyep = ord(self.ser.read(1))
+                is_extended_id = tyep & 0x20
+                is_remote_frame = tyep & 0x10
+                dlc = (tyep << 4 & 0xFF) >> 4
+                arb_id = (
+                    struct.unpack(
+                        "<I" if is_extended_id else "<H",
+                        bytearray(self.ser.read(4 if is_extended_id else 2)),
+                    )
+                )[0]
+                data = self.ser.read(dlc)
+                rxd_byte = ord(self.ser.read(1))
+                if rxd_byte == 0x55:
+                    msg = Message(
+                        arbitration_id=arb_id,
+                        timestamp=time.perf_counter(),
+                        dlc=dlc,
+                        data=data,
+                        is_extended_id=is_extended_id,
+                        is_remote_frame=is_remote_frame,
+                    )
+                    return msg, False
         except serial.SerialException:
-            return None, False
-
-        if rx_byte and ord(rx_byte) == 0xAA:
-            tyep = ord(self.ser.read(1))
-            is_extended_id = tyep & 0x20
-            is_remote_frame = tyep & 0x10
-            dlc = (tyep << 4 & 0xFF) >> 4
-            arb_id = (
-                struct.unpack(
-                    "<I" if is_extended_id else "<H",
-                    bytearray(self.ser.read(4 if is_extended_id else 2)),
-                )
-            )[0]
-            data = self.ser.read(dlc)
-            rxd_byte = ord(self.ser.read(1))
-            if rxd_byte == 0x55:
-                msg = Message(
-                    arbitration_id=arb_id,
-                    timestamp=time.perf_counter(),
-                    dlc=dlc,
-                    data=data,
-                    is_extended_id=is_extended_id,
-                    is_remote_frame=is_remote_frame,
-                )
-                return msg, False
-
-        else:
-            return None, False
+            pass
+        return None, False
 
     def fileno(self):
         if hasattr(self.ser, "fileno"):
